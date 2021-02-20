2d4445254e79cff1cc4deb2d22e6fc6ff5073d2e
Marina Reimler <marinibambini@gmail.com>
2021-02-19 21:31:01 -0800

    Add standard validation and error message with react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
 import './App.css';
 import RegistrationForm from './components/Forms/RegistrationForm';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  
   return (
     <div
       style={{
--- src/components/Forms/RegistrationForm.js
+++ src/components/Forms/RegistrationForm.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import useForm from "react-hook-form";
+import { useForm } from 'react-hook-form';
 import './Forms.css';
 import SignUpButton from '../Buttons/SignUpButton';
 import ToggleToSignIn from '../Buttons/ToggleToSignIn';
 
 function RegistrationForm () {
+  const {
+    register,
+    handleSubmit,
+    errors,
+    setError,
+    clearError,
+    formState: { isSubmitting }
+  } = useForm();
   const onSubmit = data => {
     alert(JSON.stringify(data));
-  }
+  };  
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  const validateUserName = async value => {
+    await sleep(1000);
+    if (value !== "bill") {
+      setError("username", "validate");
+    } else {
+      clearError("username");
+    }
+  };
+  
     return (
       <div className="container">
       <h1>Hello</h1>
-      <form className="form-control" onSubmit={onSubmit}>
+      <form className="form-control" onSubmit={handleSubmit(onSubmit)}>
         <div>
             <label htmlFor="email">Email</label>
             <input 
@@ -20,6 +38,7 @@ function RegistrationForm () {
             id="email" 
             placeholder="Email" 
             name="Email"
+            ref={register({ required: true })}
             />
         </div>
           <div>
@@ -29,6 +48,7 @@ function RegistrationForm () {
               id="password"
               placeholder="Password"
               name="Password"
+              ref={register}
               />
            </div>
           <div>
@@ -38,6 +58,7 @@ function RegistrationForm () {
               id="password2"
               placeholder="Confirm password"
               name="Repeat Password"
+              ref={register}
              />
           </div>
         <SignUpButton />
@@ -46,5 +67,6 @@ function RegistrationForm () {
       </div>
     )
 }
+
 
 export default RegistrationForm
